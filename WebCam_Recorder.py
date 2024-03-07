59cacc5339b10c3cd2bf91c08b15d00f8b5541e2
ajbuwidnnfjwj <416tommy@naver.com>
2024-03-08 08:52:14 +0900

    no camera exception

--- WebCam_Recorder.py
+++ WebCam_Recorder.py
@@ -1,6 +1,7 @@
 import cv2 as cv
+import numpy as np
 
-path = './record.avi'
+vid_num = 0
 fourcc = 'XVID'
 webcam = cv.VideoCapture(0)
 out = cv.VideoWriter()
@@ -9,28 +10,32 @@ while webcam.isOpened():
     status, frame = webcam.read()
 
     if status:
-        cv.imshow("test", frame)
+        cv.imshow("WebCam Recorder", frame)
 
     key = cv.waitKey(10)
     if key == 27:
         break
     elif key == ord(' '):
         h, w, *_ = frame.shape
-        out.open(path, cv.VideoWriter_fourcc(*fourcc), webcam.get(cv.CAP_PROP_FPS), (w, h))
+        out.open('./record{}.avi'.format(vid_num), cv.VideoWriter_fourcc(*fourcc), webcam.get(cv.CAP_PROP_FPS), (w, h))
         while key != 27:
             status, frame = webcam.read()
-            cv.circle(frame, (w//2,10), radius=10, color=(0, 0, 255), thickness=5)
+            cv.circle(frame, (w//2,10), radius=5, color=(0, 0, 255), thickness=5)
             if status:
-                cv.imshow("test", frame)
+                cv.imshow("WebCam Recorder", frame)
                 out.write(frame)
 
             key = cv.waitKey(10)
             if key == ord(' '):
+                vid_num += 1
                 break
         else:
             break
-
 else:
-    print("?")
+    canvas = np.full((120, 60), 255, dtype=np.uint8)
+    cv.putText(canvas, 'Cannot find camera', (canvas.shape[1]//2, canvas.shape[0]//2), cv.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0))
+    cv.waitKey()
+    cv.destroyAllWindows()
     exit()
+
 out.release()
