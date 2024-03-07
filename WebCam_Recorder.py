fb86f20f7d835da111455251e00f01371ce75a60
ajbuwidnnfjwj <416tommy@naver.com>
2024-03-07 10:09:27 +0900

    recording signal

--- WebCam_Recorder.py
+++ WebCam_Recorder.py
@@ -4,6 +4,7 @@ path = './record.avi'
 fourcc = 'XVID'
 webcam = cv.VideoCapture(0)
 out = cv.VideoWriter()
+
 while webcam.isOpened():
     status, frame = webcam.read()
 
@@ -16,13 +17,19 @@ while webcam.isOpened():
     elif key == ord(' '):
         h, w, *_ = frame.shape
         out.open(path, cv.VideoWriter_fourcc(*fourcc), webcam.get(cv.CAP_PROP_FPS), (w, h))
-        while True:
+        while key != 27:
             status, frame = webcam.read()
+            cv.circle(frame, (w//2,10), radius=10, color=(0, 0, 255), thickness=5)
             if status:
                 cv.imshow("test", frame)
                 out.write(frame)
-            if cv.waitKey(10) == ord(' '):
+
+            key = cv.waitKey(10)
+            if key == ord(' '):
                 break
+        else:
+            break
+
 else:
     print("?")
     exit()
